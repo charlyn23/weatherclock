f8b0255fc3cf57b4c7c8c5bcd0376e8c9095d095
sufeiiz <szhao2@gmail.com>
2015-04-08 22:55:10 -0400

    Finished Sufei's alarm

--- src/nyc/c4q/ac21/weatherclock/Alarm.java
+++ src/nyc/c4q/ac21/weatherclock/Alarm.java
@@ -38,43 +38,17 @@ public class Alarm {
             return AnsiTerminal.Color.MAGENTA;
     }
 
-    public static void setAlarm(Calendar cal, String alarm, int hour, int min, int sec){
-        String[] time = new String[2];
-        String ap;
-        boolean am = true;
+    public static void setAlarm(Calendar cal, boolean am, String[] time, int hour, int min, int sec){
+        boolean morning = am && (cal.get(Calendar.HOUR_OF_DAY) <= 12);
+        boolean evening = !am && (cal.get(Calendar.HOUR_OF_DAY) >= 12);
 
-        if (!alarm.contains(":") || alarm.length() < 6)
-            alarm = null;
-        else {
-            time = alarm.split(":");
-            ap = time[1].substring(2);
-            time[1] = time[1].substring(0, 2);
-            if (Integer.valueOf(time[0]) > 12 ||
-                    Integer.valueOf(time[1]) > 59)
-                alarm = null;
-            else if (time[0].length() > 2 || time[1].length() > 2)
-                alarm = null;
-            if (ap.equalsIgnoreCase("am"))
-                am = true;
-            else if (ap.equalsIgnoreCase("pm"))
-                am = false;
-        }
-
-        if (alarm != null) {
-            boolean morning = am && (cal.get(Calendar.HOUR_OF_DAY) <= 12);
-            boolean evening = !am && (cal.get(Calendar.HOUR_OF_DAY) >= 12);
-
-            terminal.moveTo(numRows, 0);
-            terminal.write("\u23F0" + "  " + alarm);
-
-            if ((morning || evening) &&
-                    hour == Integer.valueOf(time[0]) &&
-                    min == Integer.valueOf(time[1]) &&
-                    sec <= 5) {
-                Alarm.alarm();
-                terminal.setTextColor(Alarm.colorChange());
-            } else
-                terminal.clear();
-        }
+        if ((morning || evening) &&
+                hour == Integer.valueOf(time[0]) &&
+                min == Integer.valueOf(time[1]) &&
+                sec <= 5) {
+            Alarm.alarm();
+            terminal.setTextColor(Alarm.colorChange());
+        } else
+            terminal.clear();
     }
 }
